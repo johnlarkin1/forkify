7f2ac40bcc1ccb90c539fa097da112dc78d7e35e
John Larkin <john@johnjlarkin.com>
2021-10-14 12:41:56 -0400

    adding welcome

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  console.log('Welcome to the application!!');
 };
 init();
