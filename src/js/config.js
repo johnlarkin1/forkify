43545308d77d96d3826ea57e97b2c61fdcbcc44c
John Larkin <john@johnjlarkin.com>
2021-10-14 12:46:04 -0400

    updated API key

--- src/js/config.js
+++ src/js/config.js
@@ -4,5 +4,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const API_KEY = '2303f7ae-709f-4175-90f6-12f5c8900df2';
+export const API_KEY = '058db3f8-f08f-40cc-bf16-b269c78e38b3';
 export const MODAL_CLOSE_SEC = 2.5;
